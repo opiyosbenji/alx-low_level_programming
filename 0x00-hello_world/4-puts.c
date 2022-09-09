76c86d93a9d7c874eaa29717eb3b382cfcd1e45c
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-09-09 02:26:28 +0000

    uses puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Decsription: 'uses the function puts'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
