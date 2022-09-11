238c9db9025b326a5e0d35f2dfa78caa894b05c3
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-09-11 08:04:00 +0000

    Prints out the last part of a quote in the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints out the last part part of a quote.
+ *
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
