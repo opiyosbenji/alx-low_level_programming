ebc64756ee0ec834ec202a82e481538333ceba01
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-09-13 02:50:31 +0000

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			put char(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
