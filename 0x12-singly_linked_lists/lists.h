009afbdcb054aa2121189cbb091b22fd05e259a7
b3njamin-op1yo <benjaminopiyo2001@gmail.com>
2022-10-25 08:03:45 +0000

    finished the lish.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,31 +1,30 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
+#ifndef SINGLY_LINKED_LIST_H_
+#define SINGLY_LINKED_LIST_H_
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
+#include <unistd.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string
- * @len: lenght of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* for Holberton project
+*/
 
-typeof struct list_s
+typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
-} list_s;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* #ifndef SINGLY_LINKED_LIST_H_ */
